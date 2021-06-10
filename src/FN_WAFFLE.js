708dfccd903edf351048da460d6294db6b689888
Spaceman <rionlion100@gmail.com>
2021-06-09 22:00:22 -0400

    Add trader levels and flea access

--- src/FN_WAFFLE.js
+++ src/FN_WAFFLE.js
@@ -6,9 +6,11 @@ const config = require("../config.json");
 class FN_WAFFLE {
 	constructor() {
         SpaceApi.CreateNewTrader("FN_WAFFLE", true, "/files/trader/avatar/FN_WAFFLE.png", "RUB");
-        SpaceApi.CreateNewTraderLocale("en", "FN_WAFFLE", "FN Waffle", "FN", "FN Waffle", "Belgium", "Vendor for all of FN-Herstal's Products")
-
-        
+        SpaceApi.CreateNewTraderLocale("en", "FN_WAFFLE", "FN Waffle", "FN", "FN Waffle", "Belgium", "Vendor for all of FN-Herstal's Products");
+        SpaceApi.EditTraderLL("FN_WAFFLE", "1", 15, 250000, 0);
+        SpaceApi.EditTraderLL("FN_WAFFLE", "2", 25, 500000, 0);
+        SpaceApi.EditTraderLL("FN_WAFFLE", "3", 35, 1000000, 0);
+        RagfairConfig.traders["FN_WAFFLE"] = true;
 
         this.funcptr = HttpServer.onRespond["IMAGE"];    
         HttpServer.onRespond["IMAGE"] = this.getImage.bind(this);
